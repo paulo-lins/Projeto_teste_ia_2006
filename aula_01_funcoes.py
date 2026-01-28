b8fd8c259b3a012d7fcfcb8683ba8812ee48f9ec
Paulo Lins <paulolins100@gmail.com>
2026-01-28 14:04:59 -0300

    Melhorando a funçao

--- aula_01_funcoes.py
+++ aula_01_funcoes.py
@@ -1,14 +1,31 @@
-#Função simula o que o modelo faz
-def meu_primeiro_embedding(text_do_pdf):
-    #aqui ficaria a IA que transforma texto em números
-    lista_de_numeros = [0.1, 0.2, 0.3]
-    return lista_de_numeros
 
-#Usamos a função aqui
-entrada = "Relatório Apple 2026"
-resultado = meu_primeiro_embedding(entrada)
+#%%
+#1 Função "Motor"melhorada
+def processador_de_ia(texto):
+    #simula a IA gerando um vetor de 3 números
+    #e coloca em letra maiúscula para vermos a mudança
+    vetor_simulado = [0.1, 0.2, 0.3]
+    return f"VETOR: {vetor_simulado} | DOC: {texto.upper()}"
 
-print(f"Texto enviado: {entrada}")
-print(f"Vetor gerado: {resultado}")
+# 2. Uma lista de documentos (Simulando várias páginas de um PDF)
+documentos = [
+    "Receita do primeiro trimestre",
+    "Custos de fabricação do iPhone",
+    "Investimentos em Inteligência Artificial",
+    "Projeções para 2026"
+]
+
+# Loop for
+print("-----iniciando processamento-------")
+
+count = 0
+for doc in documentos:
+    count == count + 1
+    resultado = processador_de_ia(doc)
+    print(f"Processado {count} com sucesso: {resultado}")
+    
 
 
+
+
+# %%
