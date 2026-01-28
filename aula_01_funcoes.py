11d6a6cd24bddb31c98a9d5571eafaeec377f523
Paulo Lins <paulolins100@gmail.com>
2026-01-28 13:36:59 -0300

    Iniciando estudos de funções de IA

---
+++ aula_01_funcoes.py
@@ -0,0 +1,14 @@
+#Função simula o que o modelo faz
+def meu_primeiro_embedding(text_do_pdf):
+    #aqui ficaria a IA que transforma texto em números
+    lista_de_numeros = [0.1, 0.2, 0.3]
+    return lista_de_numeros
+
+#Usamos a função aqui
+entrada = "Relatório Apple 2026"
+resultado = meu_primeiro_embedding(entrada)
+
+print(f"Texto enviado: {entrada}")
+print(f"Vetor gerado: {resultado}")
+
+
